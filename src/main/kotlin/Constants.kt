058813deadcf66b8cb705b3a1bcd4e2c7aa3e53b
Sauce Chili <71555556+sauce-chili@users.noreply.github.com>
2024-06-08 16:56:29 +0300

    Changed connection parameters to work only with the test database

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,7 @@
+import data.source.postresimpl.ConnectionParam
+
+val ConnectionParamPostgres = ConnectionParam(
+    url = "jdbc:postgresql://localhost:5432/SmallTicketBookingDB",
+    user = "postgres",
+    password = "008972",
+)
